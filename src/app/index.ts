afcad5fdb135d901b50476a2eeae1e807d10522f
lucianobs2 <luciano_bs1@outlook.com>
2026-04-14 21:44:08 -0300

    feat: install and config mongodb

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,15 @@
+import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
-app.listen(3001, () => {
-  console.log('💫 Server is running on Port 3001 💫');
-});
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    console.log('Connection mongo ok');
+
+    const app = express();
+    app.listen(process.env.PORT, () => {
+      console.log(`💫 Server is running on port ${process.env.PORT} 💫`);
+    });
+  })
+  .catch(() => console.log('Error on connect mongodb'));
